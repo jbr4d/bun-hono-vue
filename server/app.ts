15b462cc1e8d3692654eea4226c6dc45f831ac8b
Jacob Bradley <jtbradley1@gmail.com>
2024-09-06 07:06:18 -0700

    build: create bun, hono, zod app

---
+++ server/app.ts
@@ -0,0 +1,19 @@
+import { Hono } from 'hono'
+import { logger } from 'hono/logger'
+import { expensesRoute } from './routes/expenses'
+
+const app = new Hono()
+
+app.use(logger())
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('/test', (c) => {
+    return c.json({'message': 'yo'})
+  })
+
+app.route('api/expenses', expensesRoute)
+
+export default app
---
+++ server/routes/expenses.ts
@@ -0,0 +1,52 @@
+import { Hono } from 'hono'
+import { z } from 'zod'
+import { zValidator } from '@hono/zod-validator'
+
+const expenseSchema = z.object({
+    id: z.number().int().positive.min(1),
+    title: z.string().min(3).max(100),
+    amount: z.number().int().positive()
+})
+
+type Expense = z.infer<typeof expenseSchema>
+
+const createPostSchema = z.object({
+    title: z.string().min(3).max(100),
+    amount: z.number().int().positive()
+})
+
+
+const fakeExpenses = [
+    { id: 1, title: "Groceries", amount: 50},
+    { id: 2, title: "Utlilities", amount: 100},
+    { id: 3, title: "Rent", amount: 1200},
+]
+
+export const expensesRoute = new Hono()
+.get('/', (c) => {
+    return c.json({ expenses: fakeExpenses})
+})
+.post('/', zValidator('json', createPostSchema), async (c) => {
+    const expense = await c.req.valid('json')
+    fakeExpenses.push({...expense, id: fakeExpenses.length+1})
+    c.status(201)
+    return c.json(expense)
+})
+.get('/:id{[0-9]+}', (c) => {
+    const id = Number.parseInt(c.req.param('id'))
+    const expense = fakeExpenses.find(expense => expense.id === id)
+    if (!expense) {
+        return c.notFound()
+    }
+    return c.json({expense})
+})
+.delete('/:id{[0-9]+}', (c) => {
+    const id = Number.parseInt(c.req.param('id'))
+    const expense = fakeExpenses.find(expense => expense.id === id)
+    if (!expense) {
+        return c.notFound()
+    }
+    const deletedExpense = fakeExpenses.splice(index, 1)[0]
+    return c.json({expense: deletedExpense})
+})
+// .put
