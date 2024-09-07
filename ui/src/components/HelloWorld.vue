3f1d8378d60517296b722f3e3b75efe9d7db2aed
Jacob Bradley <jtbradley1@gmail.com>
2024-09-07 10:14:09 -0700

    feat: add `<Button>`

--- ui/src/components/HelloWorld.vue
+++ ui/src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import { Button } from './ui/button';
 
 defineProps<{ msg: string }>()
 
@@ -10,8 +11,8 @@ const count = ref(0)
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++" class="bg-secondary hover:bg-background">Up</button>
-    <button type="button" @click="count--">Down</button>
+    <Button type="button" @click="count++">Up</Button>
+    <Button type="button" @click="count--">Down</Button>
     {{  count }}
   </div>
 </template>
