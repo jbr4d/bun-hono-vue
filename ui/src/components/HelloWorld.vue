61a98a8b802c874c001aa9188a6c70f7cd2f3ca9
Jacob Bradley <jtbradley1@gmail.com>
2024-09-07 10:24:22 -0700

    feat: add `<Card>`

--- ui/src/components/HelloWorld.vue
+++ ui/src/components/HelloWorld.vue
@@ -1,18 +1,39 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { Button } from './ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
 
 defineProps<{ msg: string }>()
 
 const count = ref(0)
+
+const totalSpent = ref(0)
+
 </script>
 
 <template>
+
+  <Card class="w-96 mx-auto">
+    <CardHeader>
+      <CardTitle>Total Spent</CardTitle>
+      <CardDescription>Total amount you've spent</CardDescription>
+    </CardHeader>
+    <CardContent>
+      {{ totalSpent }}
+    </CardContent>
+  </Card>
+
   <h1>{{ msg }}</h1>
 
   <div class="card">
     <Button type="button" @click="count++">Up</Button>
     <Button type="button" @click="count--">Down</Button>
-    {{  count }}
+    {{ count }}
   </div>
 </template>
