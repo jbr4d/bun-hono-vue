9b0890b3ae79513e50e4475cb584f2fad6ec9862
Jacob Bradley <jtbradley1@gmail.com>
2024-09-07 11:22:07 -0700

    feat: route frontend /dist from server

--- server/app.ts
+++ server/app.ts
@@ -1,19 +1,15 @@
 import { Hono } from 'hono'
 import { logger } from 'hono/logger'
+import { serveStatic } from 'hono/bun'
 import { expensesRoute } from './routes/expenses'
 
 const app = new Hono()
 
 app.use(logger())
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
-
-app.get('/test', (c) => {
-    return c.json({'message': 'yo'})
-  })
-
 app.route('api/expenses', expensesRoute)
+
+app.get('*', serveStatic({ root: './ui/dist' }))
+app.get('*', serveStatic({ path: './ui/dist/index.html' }))
 
 export default app
