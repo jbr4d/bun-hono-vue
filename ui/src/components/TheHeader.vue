e0c1486ef11f256ebb1b77b000ac8b99dd66fdbd
Jacob Bradley <jtbradley1@gmail.com>
2024-09-18 23:09:30 -0700

    feat: add `<TheHeader>`

---
+++ ui/src/components/TheHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <header class="p-4">
+        <nav class="flex space-x-4">
+            <RouterLink icon-btn to="/">
+            Home
+            </RouterLink>
+
+            <RouterLink icon-btn to="/about">
+            About
+            </RouterLink>
+
+            <RouterLink icon-btn to="/expenses">
+                Expenses
+            </RouterLink>
+
+            <RouterLink icon-btn to="/create-expense">
+                Create Expense
+            </RouterLink>
+
+        </nav>
+    </header>
+</template>
