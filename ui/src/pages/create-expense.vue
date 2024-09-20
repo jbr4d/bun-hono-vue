22c332e93a5e35bceb4965f2a83eef11c63ef77b
Jacob Bradley <jtbradley1@gmail.com>
2024-09-20 01:30:46 -0700

    feat: create expese container

--- ui/src/pages/create-expense.vue
+++ ui/src/pages/create-expense.vue
@@ -17,7 +17,7 @@ const form = useForm({
     // Do something with form data
     const res = await api.expenses.$post({ json: value })
     if (!res.ok) {
-        throw new Error('server error')
+      throw new Error('server error')
     }
     router.push('/')
   },
@@ -26,54 +26,40 @@ const form = useForm({
 </script>
 
 <template>
-    <div class="py-4">Create an Expense</div>
 
-    <div>
-    <form @submit="
-      (e) => {
+  <div class="max-w-md mx-auto">
+    <form @submit="(e) => {
         e.preventDefault()
         e.stopPropagation()
         form.handleSubmit()
       }
-    "
-    class="space-y-2">
+      " class="space-y-2">
       <div>
         <form.Field name="title">
           <template v-slot="{ field }">
             <label :for="field.name">Title:</label>
-            <Input
-              :name="field.name"
-              :value="field.state.value"
-              @blur="field.handleBlur"
-              @input="(e) => field.handleChange((e.target as HTMLInputElement).value)"
-            />
+            <Input :name="field.name" :value="field.state.value" @blur="field.handleBlur"
+              @input="(e) => field.handleChange((e.target as HTMLInputElement).value)" />
           </template>
         </form.Field>
       </div>
       <div>
-        <form.Field name="amount"
-        :validators="{
-            onBlur: ({ value }) => value < 200 ? 'Must be more than 200' : undefined,
+        <form.Field name="amount" :validators="{
+          onBlur: ({ value }) => value < 200 ? 'Must be more than 200' : undefined,
         }">
           <template v-slot="{ field }">
             <label :for="field.name">Amount:</label>
-            <Input
-              :name="field.name"
-              :value="field.state.value"
-              type="number"
-              @blur="field.handleBlur"
-              @input="(e) => field.handleChange((e.target as HTMLInputElement).valueAsNumber)
-                "
-            />
+            <Input :name="field.name" :value="field.state.value" type="number" @blur="field.handleBlur" @input="(e) => field.handleChange((e.target as HTMLInputElement).valueAsNumber)
+              " />
             <em role="alert" v-if="field.state.meta.errors">{{ field.state.meta.errors.join(', ') }}</em>
           </template>
         </form.Field>
       </div>
       <form.Subscribe>
         <template v-slot="{ canSubmit, isSubmitting }">
-      <Button type="submit" :disabled="!canSubmit">{{ isSubmitting ? '...' : 'Submit' }}</Button>
-    </template>
-    </form.Subscribe>
+          <Button type="submit" :disabled="!canSubmit">{{ isSubmitting ? '...' : 'Submit' }}</Button>
+        </template>
+      </form.Subscribe>
     </form>
   </div>
 </template>
